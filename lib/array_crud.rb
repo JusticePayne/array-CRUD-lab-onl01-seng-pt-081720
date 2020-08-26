2be217050dfd5a4794960bc8c5040fc582f1a0ca
Justice Payne <spikepayne@yahoo.com>
2020-08-26 01:18:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+    family = ["James","Justice","Greyson","Arwen"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(family, jax)
+  family.push(Jax)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(family, allport)
+    family.unshift(allport)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(family)
+  family.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(family)
+  family.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(array, element)
+  array[]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(family)
+  family[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(family)
+  family[4]
 end
