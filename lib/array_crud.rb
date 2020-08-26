d717d4b907da1efb633469f9629117f382d42853
Justice Payne <spikepayne@yahoo.com>
2020-08-26 01:52:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  element.pop
+  array.pop
 end
 
-def remove_element_from_start_of_array(family)
-  family.shift
+def remove_element_from_start_of_array(array)
+  array.shift
 end
 
 def retrieve_element_from_index(array, element)
